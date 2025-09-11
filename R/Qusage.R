6755743aac128dc87c49609595e8b0b7ae9e70e5
poulainn <45354913+poulainn@users.noreply.github.com>
2025-09-11 15:45:37 +0100

    Update Qusage.R

--- R/Qusage.R
+++ R/Qusage.R
@@ -1,1 +1,15 @@
 
+read.gmt <- function (file) 
+{
+    if (!grepl("\\.gmt$", file)[1]) {
+        stop("Pathway information must be a .gmt file")
+    }
+    geneSetDB = readLines(file)
+    geneSetDB = strsplit(geneSetDB, "\t")
+    names(geneSetDB) = sapply(geneSetDB, "[", 1)
+    geneSetDB = lapply(geneSetDB, "[", -1:-2)
+    geneSetDB = lapply(geneSetDB, function(x) {
+        x[which(x != "")]
+    })
+    return(geneSetDB)
+}
